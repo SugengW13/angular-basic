2ae04408cbf900a261fd2798105f62fd5d42a256
Sugeng Warsito <sugengwarsito131001@gmail.com>
2023-07-20 12:59:06 +0700

    feat: create service hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
@@ -8,9 +9,19 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  constructor(private heroService: HeroService) {}
+
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
   }
 }
