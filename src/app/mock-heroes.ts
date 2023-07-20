643e6a5c038200a7f1d2716a796da2d382bc1004
Sugeng Warsito <sugengwarsito131001@gmail.com>
2023-07-20 11:42:53 +0700

    feat: create hero component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'BigChunggus' },
+  { id: 2, name: 'Seno Marseno' },
+  { id: 3, name: 'Zongli' },
+  { id: 4, name: 'Dio Brando' },
+  { id: 5, name: 'Jotaro Kujo' }
+]
