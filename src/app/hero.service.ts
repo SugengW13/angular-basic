6d93353ea22113eded127c972c6ac7a144f778aa
Sugeng Warsito <sugengwarsito131001@gmail.com>
2023-07-20 13:23:55 +0700

    feat: create component & service messages

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
+
 import { Observable, of } from 'rxjs';
+
+import { MessageService } from './message.service';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
@@ -7,11 +11,11 @@ import { HEROES } from './mock-heroes';
   providedIn: 'root'
 })
 export class HeroService {
-
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fethced heroes')
     return heroes
   }
 }
